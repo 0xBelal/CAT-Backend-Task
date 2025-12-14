495bc5acf87078414972f4b74b8375de38e23feb
Belal <0xbelalmhmd@gmail.com>
2025-12-14 03:53:20 +0200

    add task class

---
+++ 1. To-Do List CLI/Task.h
@@ -0,0 +1,34 @@
+//
+// Created by belal on 12/14/25.
+//
+
+#ifndef INC_1__TO_DO_LIST_CLI_TASK_H
+#define INC_1__TO_DO_LIST_CLI_TASK_H
+#include <string>
+
+
+class Task
+{
+
+private:
+    string id;
+    string title;
+    string description;
+
+public:
+
+    Task(){}
+    Task(string id,string title,string desc) : id(id), title(title), description(desc){}
+
+    string get_id(){return id;}
+    string get_title(){return title;}
+    string get_description(){return description;}
+
+    void set_id(string id){  this->id = id;}
+    void set_title(string title){ this->title = title;}
+    void set_description(string desc){this->description = desc;}
+
+};
+
+
+#endif //INC_1__TO_DO_LIST_CLI_TASK_H
