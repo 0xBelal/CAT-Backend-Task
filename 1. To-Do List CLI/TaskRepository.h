30a600be118e6141f38323d1489364e04386469d
Belal <0xbelalmhmd@gmail.com>
2025-12-14 10:30:17 +0200

    add TaskRepository class

--- 1. To-Do List CLI/TaskRepository.h
+++ 1. To-Do List CLI/TaskRepository.h
@@ -1,15 +1,17 @@
 //
 // Created by belal on 12/14/25.
 //
-
+#include <vector>
+#include "Task.h"
+#include <vector>
 #ifndef INC_1__TO_DO_LIST_CLI_TASKREPOSITORY_H
 #define INC_1__TO_DO_LIST_CLI_TASKREPOSITORY_H
 
 class TaskRepository
 {
     public:
-    virtual void add(const Task& task) = 0;
-    virtual std::vector<Task> getAll() = 0;
+    virtual bool add (Task task) = 0;
+    virtual vector<Task> getAll() = 0;
     virtual ~TaskRepository() = default;
 
 };
